60dd6882dc6506b65581420f2060f1a4d102b50c
AndriKls <158137294+AndriKls@users.noreply.github.com>
2024-10-14 16:07:14 +0300

    init + startpage

---
+++ EstonianAnimals/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EstonianAnimals
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ EstonianAnimals/StartPage.xaml.cs
@@ -0,0 +1,16 @@
+namespace MauiApp1.Views
+{
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        async void ExploreNow_Clicked(object sender, EventArgs e)
+        {
+            // Navigate to the main application page (e.g., PlanetsPage or AnimalsPage)
+            
+        }
+    }
+}
