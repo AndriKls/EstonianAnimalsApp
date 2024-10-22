b894716ce837cdf7c4f8359c95a84904be4cca98
PRÄÄNIK\ander <Ander.sinisalu@gmail.com>
2024-10-22 15:45:55 +0300

    BirdsDescriptions

--- EstonianAnimals/Views/BirdsPage.xaml.cs
+++ EstonianAnimals/Views/BirdsPage.xaml.cs
@@ -14,7 +14,11 @@ namespace EstonianAnimals.Views
 
             Birds = new ObservableCollection<Animal>
             {
-                new Animal { Name = "Rasvatihane", Image = "", Description = "" },
+                new Animal { Name = "Rasvatihane", Image = "greattit.png", Description = "Rasvatihane on linnuliik tihaslaste sugukonnast. Ta on Eesti ¸ks tavalisemaid ja tuntumaid linde ning ¸ks pıhjalikumalt uuritud linnuliike maailmas. Lisaks on ta Eestis arvukaim talvitaja. Rasvatihane oli Eesti 2016. aasta lind." },
+                new Animal { Name = "Suitsup‰‰suke", Image = "barnswallow.png", Description = "Suitsup‰‰suke on p‰‰sulaste sugukonda kuuluv v‰ike r‰ndlind. Suitsup‰‰suke on kıige suurema levilaga p‰‰sulane. Ta on levinud haudelind Euroopas, Aasias, Pıhja-Aafrikas ja Pıhja-Ameerikas. Suitsup‰‰suke on Eesti ja oli aastatel 1962ñ1964 Austria rahvuslind." },
+                new Animal { Name = "Koduvarblane", Image = "housesparrow.png", Description = "Koduvarblane on linnuliik v‰rvuliste seltsist, varblaslaste sugukonnast. Koduvarblane on looduslikult levinud valdavas osas Euraasias. Ta on inimkaasleja ja teda on alates 19. sajandi keskpaigast introdutseeritud Ameerikasse, Aafrikasse Saharast lıuna pool, Uus-Meremaale ja Austraaliasse ning paljudesse maailma linnadesse, et asendada v‰ljar‰nnanud liike." },
+                new Animal { Name = "Hallvares", Image = "hoodedcrow.png", Description = "Hallvares on v‰rvuliste seltsi vareslaste sugukonda kuuluv liik linde. Hallvares on levinud Euraasias Iirimaast, äotimaast, Skandinaaviast ja Mandri-Euroopa kesk- ja lıunaosadest kuni L‰‰ne-Siberini. Hallvareste arvukus aasta-aastalt kasvab ja paljude teiste linnalindude arvukus selle vırra v‰heneb." },
+                new Animal { Name = "Suur-kirjur‰hn", Image = "greatspottedwoodpecker.png", Description = "Suur-kirjur‰hn on Eesti levinuim r‰hniliik. Kuulub kirjur‰hni perekonda. Suur-kirjur‰hni pesitsusaegset arvukust hinnatakse 50 000 ñ 100 000 paarile, talvist arvukust 50 000 ñ 300 000 isendile." },
             };
 
             BindingContext = this;
